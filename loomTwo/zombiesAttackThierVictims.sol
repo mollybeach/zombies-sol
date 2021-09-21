06be63e014bc17edd9066d0b3adbde0d56e364a3
mollybeach <mollyjbeach@gmail.com>
2021-09-21 14:55:20 -0700

    loom2 #4 completed

--- loomTwo/zombiesAttackThierVictims.sol
+++ loomTwo/zombiesAttackThierVictims.sol
@@ -33,6 +33,8 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
+
+        require(ownerZombieCount[msg.sender] == 0); //if the owner has a zombie, they can't create a new one if the value is not 0 the next line will fail and give an error
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
@@ -89,49 +91,60 @@ contract ZombieFactory {
     uint dnaModulus = 10 ** dnaDigits;
     struct Zombie {
 Zombies Attack Their Victims
-Zombies Attack Thei
+
+
 Chapter 3: Msg.sender
 Now that we have our mappings to keep track of who owns a zombie, we'll want to update the _createZombie method to use them.
-
 In order to do this, we need to use something called msg.sender.
-
 msg.sender
 In Solidity, there are certain global variables that are available to all functions. One of these is msg.sender, which refers to the address of the person (or smart contract) who called the current function.
-
 In Solidity, function execution always needs to start with an external caller. A contract will just sit on the blockchain doing nothing until someone calls one of its functions. So there will always be a msg.sender.
-
 Here's an example of using msg.sender and updating a mapping:
 mapping (address => uint) favoriteNumber;
-
 function setMyNumber(uint _myNumber) public {
   // Update our `favoriteNumber` mapping to store `_myNumber` under `msg.sender`
   favoriteNumber[msg.sender] = _myNumber;
   // ^ The syntax for storing data in a mapping is just like with arrays
 }
-
 function whatIsMyNumber() public view returns (uint) {
   // Retrieve the value stored in the sender's address
   // Will be `0` if the sender hasn't called `setMyNumber` yet
   return favoriteNumber[msg.sender];
 }
 In this trivial example, anyone could call setMyNumber and store a uint in our contract, which would be tied to their address. Then when they called whatIsMyNumber, they would be returned the uint that they stored.
-
 Using msg.sender gives you the security of the Ethereum blockchain — the only way someone can modify someone else's data would be to steal the private key associated with their Ethereum address.
-
 Put it to the test
 Let's update our _createZombie method from lesson 1 to assign ownership of the zombie to whoever called the function.
-
 First, after we get back the new zombie's id, let's update our zombieToOwner mapping to store msg.sender under that id.
-
 Second, let's increase ownerZombieCount for this msg.sender.
-
 In Solidity, you can increase a uint with ++, just like in javascript:
-
 uint number = 0;
 number++;
 // `number` is now `1`
 Your final answer for this chapter should be 2 lines of code.
 
+
+Chapter 4: Require
+In lesson 1, we made it so users can create new zombies by calling createRandomZombie and entering a name. However, if users could keep calling this function to create unlimited zombies in their army, the game wouldn't be very fun.
+Let's make it so each player can only call this function once. That way new players will call it when they first start the game in order to create the initial zombie in their army.
+How can we make it so this function can only be called once per player?
+For that we use require. require makes it so that the function will throw an error and stop executing if some condition is not true:
+function sayHiToVitalik(string memory _name) public returns (string memory) {
+  // Compares if _name equals "Vitalik". Throws an error and exits if not true.
+  // (Side note: Solidity doesn't have native string comparison, so we
+  // compare their keccak256 hashes to see if the strings are equal)
+    require(keccak256(abi.encodePacked(_name)) == keccak256(abi.encodePacked("Vitalik")));
+  // If it's true, proceed with the function:
+return "Hi!";
+If you call this function with sayHiToVitalik("Vitalik"), it will return "Hi!". If you call it with any other input, it will throw an error and not execute.
+Thus require is quite useful for verifying certain conditions that must be true before running a function.
+Put it to the test
+In our zombie game, we don't want the user to be able to create unlimited zombies in their army by repeatedly calling createRandomZombie — it would make the game not very fun.
+Let's use require to make sure this function only gets executed one time per user, when they create their first zombie.
+1. Put a require statement at the beginning of createRandomZombie. The function should check to make sure ownerZombieCount[msg.sender] is equal to 0, and throw an error otherwise.
+Note: In Solidity, it doesn't matter which term you put first — both orders are equivalent. However, since our answer checker is really basic, it will only accept one answer as correct — it's expecting ownerZombieCount[msg.sender] to come first.
+
+}
 
 
 */
