e3f5cab6ec00cc96301d68b7da0393bbc3779579
mollybeach <mollyjbeach@gmail.com>
2021-09-21 15:46:30 -0700

    loom2 #11 completed

--- loomTwo/zombiefeeding.sol
+++ loomTwo/zombiefeeding.sol
@@ -21,6 +21,9 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
     //Start here
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    // Initialize kittyContract here using `ckAddress` from above
+    KittyInterface kittyContract = KittyInterface(ckAddress);
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
--- loomTwo/zombiesattacktheirvictims.sol
+++ loomTwo/zombiesattacktheirvictims.sol
@@ -359,6 +359,28 @@ Now that we know what this function looks like, we can use it to create an inter
 1. Define an interface called KittyInterface. Remember, this looks just like creating a new contract — we use the contract keyword.
 2. Inside the interface, define the function getKitty (which should be a copy/paste of the function above, but with a semi-colon after the returns statement, instead of everything inside the curly braces.
 
+Chapter 11: Using an Interface
+Continuing our previous example with NumberInterface, once we've defined the interface as:
+contract NumberInterface {
+  function getNum(address _myAddress) public view returns (uint);
+}
+We can use it in a contract as follows:
+contract MyContract {
+  address NumberInterfaceAddress = 0xab38... 
+  // ^ The address of the FavoriteNumber contract on Ethereum
+  NumberInterface numberContract = NumberInterface(NumberInterfaceAddress);
+  // Now `numberContract` is pointing to the other contract
+
+  function someFunction() public {
+    // Now we can call `getNum` from that contract:
+    uint num = numberContract.getNum(msg.sender);
+    // ...and do something with `num` here
+  }
+}
+In this way, your contract can interact with any other contract on the Ethereum blockchain, as long they expose those functions as public or external.
+Put it to the test
+Let's set up our contract to read from the CryptoKitties smart contract!
+1. I've saved the address of the CryptoKitties contract in the code for you, under a variable named ckAddress. In the next line, create a KittyInterface named kittyContract, and initialize it with ckAddress — just like we did with numberContract above.
 
 
 */
