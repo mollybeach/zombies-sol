db868fa7bd79f7dac43e5213db86d328a981626e
mollybeach <mollyjbeach@gmail.com>
2021-09-21 13:54:26 -0700

    #11 #12

--- zombies.sol
+++ zombies.sol
@@ -10,13 +10,21 @@ contract ZombieFactory {
     }
 
     Zombie[] public zombies; 
-
+    //8 9
     function _createZombie(string memory _name, uint _dna) private{
         zombies.push(Zombie(_name, _dna)); 
     }
-   // function _generateRandomDna(string memory _str) private view returns(uint){
-    //}
+    //10 11
+    function _generateRandomDna(string memory _str) private view returns(uint){
+        uint rand = uint(keccak256(abi.encodePacked(_str))); 
+        return rand % dnaModulus;
 
+    }
+    //12
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
 }
 /*
 Chapter 9: Private / Public Functions
@@ -46,7 +54,7 @@ To return a value from a function, the declaration looks like this:
 string greeting = "What's up dog";
 
 function sayHello() public returns (string memory) {
-  return greeting;
+    return greeting;
 }
 In Solidity, the function declaration contains the type of the return value (in this case string).
 Function modifiers
@@ -78,7 +86,56 @@ contract ZombieFactory {
         uint dna;
 
 Making the Zombie Factory
+Chapter 11: Keccak256 and Typecasting
+We want our _generateRandomDna function to return a (semi) random uint. How can we accomplish this?
+Ethereum has the hash function keccak256 built in, which is a version of SHA3. A hash function basically maps an input into a random 256-bit hexadecimal number. A slight change in the input will cause a large change in the hash.
 
+It's useful for many purposes in Ethereum, but for right now we're just going to use it for pseudo-random number generation.
+Also important, keccak256 expects a single parameter of type bytes. This means that we have to "pack" any parameters before calling keccak256:
+
+Example:
+//6e91ec6b618bb462a4a6ee5aa2cb0e9cf30f7a052bb467b0ba58b8748c00d2e5
+keccak256(abi.encodePacked("aaaab"));
+//b1f078126895a1424524de5321b339ab00408010b7cf0e6ed451514981e58aa9
+keccak256(abi.encodePacked("aaaac"));
+As you can see, the returned values are totally different despite only a 1 character change in the input.
+Secure random-number generation in blockchain is a very difficult problem. Our method here is insecure, but since security isn't top priority for our Zombie DNA, it will be good enough for our purposes.
+
+Typecasting
+Sometimes you need to convert between data types. Take the following example:
+uint8 a = 5;
+uint b = 6;
+// throws an error because a * b returns a uint, not uint8:
+uint8 c = a * b;
+// we have to typecast b as a uint8 to make it work:
+uint8 c = a * uint8(b);
+In the above, a * b returns a uint, but we were trying to store it as a uint8, which could cause potential problems. By casting it as a uint8, it works and the compiler won't throw an error.
+Put it to the test
+Let's fill in the body of our _generateRandomDna function! Here's what it should do:
+The first line of code should take the keccak256 hash of abi.encodePacked(_str) to generate a pseudo-random hexadecimal, typecast it as a uint, and finally store the result in a uint called rand.
+We want our DNA to only be 16 digits long (remember our dnaModulus?). So the second line of code should return the above value modulus (%) dnaModulus.
+
+
+
+Chapter 12: Putting It Together
+We're close to being done with our random Zombie generator! Let's create a public function that ties everything together.
+We're going to create a public function that takes an input, the zombie's name, and uses the name to create a zombie with random DNA.
+Put it to the test
+Create a public function named createRandomZombie. It will take one parameter named _name (a string with the data location set to memory). (Declare this function public just as you declared previous functions private)
+The first line of the function should run the _generateRandomDna function on _name, and store it in a uint named randDna.
+The second line should run the _createZombie function and pass it _name and randDna.
+
+The solution should be 4 lines of code (including the closing } of the function).
+Contract.sol
+123456789101112131415161718192021222324
+pragma solidity  >=0.5.0 <0.6.0;
+contract ZombieFactory {
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
 
 
 */
