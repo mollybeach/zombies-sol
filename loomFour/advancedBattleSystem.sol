36774fa646485933b02631cc6d23a279300d618c
mollybeach <mollyjbeach@gmail.com>
2021-09-21 20:25:34 -0700

    loom4 #10 zombie victory function

--- loomFour/advancedBattleSystem.sol
+++ loomFour/advancedBattleSystem.sol
@@ -456,4 +456,44 @@ contract ZombieFactory is Ownable {
 attack
 1 of 3
 
+
+Chapter 10: Zombie Victory 😄
+Now that we have a winCount and lossCount, we can update them depending on which zombie wins the fight.
+
+In chapter 6 we calculated a random number from 0 to 100. Now let's use that number to determine who wins the fight, and update our stats accordingly.
+
+Put it to the test
+Create an if statement that checks if rand is less than or equal to attackVictoryProbability.
+
+If this condition is true, our zombie wins! So:
+
+a. Increment myZombie's winCount.
+
+b. Increment myZombie's level. (Level up!!!!!!!)
+
+c. Increment enemyZombie's lossCount. (Loser!!!!!! 😫 😫 😫)
+
+d. Run the feedAndMultiply function. Check zombiefeeding.sol to see the syntax for calling it. For the 3rd argument (_species), pass the string "zombie". (It doesn't actually do anything at the moment, but later we could add extra functionality for spawning zombie-based zombies if we wanted to).
+
+zombieattack.sol
+zombiehelper.sol
+zombiefeeding.sol
+zombiefactory.sol
+ownable.sol
+12345678910111213141516
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiehelper.sol";
+
+contract ZombieAttack is ZombieHelper {
+  uint randNonce = 0;
+  uint attackVictoryProbability = 70;
+
+  function randMod(uint _modulus) internal returns(uint) {
+    randNonce++;
+
+attack
+1 of 3
+
+
 */
