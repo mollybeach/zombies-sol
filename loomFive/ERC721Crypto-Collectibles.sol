2539acc7c61a8fe3f5cd1092457b6462b03e5bd2
mollybeach <mollyjbeach@gmail.com>
2021-09-21 22:26:23 -0700

    loom5 #14 Congratulations! That concludes Lesson 5. Tokens, the ERC721 standard, and tradable assets/zombies Libraries and how to use them How to prevent overflows and underflows using the SafeMath library Commenting your code and the natspec standard

--- loomFive/ERC721Crypto-Collectibles.sol
+++ loomFive/ERC721Crypto-Collectibles.sol
@@ -672,5 +672,27 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
 ERC721 & Crypto-Collectibles
 
  ERC721 & Crypto-Collectibles
+Chapter 14: Wrapping It Up
+Congratulations! That concludes Lesson 5.
 
+As a reward, we've transferred you your very own Level 10 H4XF13LD MORRIS 💯💯😎💯💯 zombie!
+
+(Omg, the legendary H4XF13LD MORRIS 💯💯😎💯💯 zombie!!!!111)
+
+Now you have 4 zombies in your army.
+
+Before you move on, you have the option to rename any of them if you'd like by clicking on them to the right and entering a new name. (Though I don't know why you would ever want to rename H4XF13LD MORRIS 💯💯😎💯💯, clearly the best name ever).
+
+Let's recap:
+In this lesson we learned about:
+
+Tokens, the ERC721 standard, and tradable assets/zombies
+Libraries and how to use them
+How to prevent overflows and underflows using the SafeMath library
+Commenting your code and the natspec standard
+This lesson concludes our game's Solidity code! (For now — we may add even more lessons in the future).
+
+In the next 2 lessons, we're going to look at how to deploy your contracts and interact with them using web3.js (so you can build a front-end for your DApp).
+
+Go ahead and rename any of your zombies if you like, then proceed to the next chapter to complete the lesson.
  */
