e261be47d8dd6a8548caaf97674f8fb67f6143ed
mollybeach <mollyjbeach@gmail.com>
2021-09-21 18:30:47 -0700

    loom3 #12 and #13 completed added have functions to update a zombie's name and DNA once the zombie gets above a certain level  protect core functions with onlyOwner added a way to update our CryptoKitties contracts  levels and cooldowns to our zombies

--- loomThree/advancedsolidityconcepts.sol
+++ loomThree/advancedsolidityconcepts.sol
@@ -650,4 +650,32 @@ No Results
 
 uint
 
+
+Chapter 13: Wrapping It Up
+Congratulations! That concludes Lesson 3.
+
+Let's recap:
+We've added a way to update our CryptoKitties contracts
+We've learned to protect core functions with onlyOwner
+We've learned about gas and gas optimization
+We added levels and cooldowns to our zombies
+We now have functions to update a zombie's name and DNA once the zombie gets above a certain level
+And finally, we now have a function to return a user's zombie army
+Claim your reward
+As a reward for completing Lesson 3, both of your zombies have leveled up!
+
+And now that NoName (the kitty-zombie you created in Lesson 2), is upgraded to level 2, you can call changeName to give him/her a name. NoName no more!
+
+Go ahead and give NoName a name, then proceed to the next chapter to complete the lesson.
+
+Your Deck
+
+ALLTROLLSGOTOHEAVENQ
+A LEVEL 3 CRYPTOZOMBIE
+
+NONAME
+A LEVEL 2 CRYPTOZOMBIE
+
+Give NoName a new name and press 'Save'
+
 */
