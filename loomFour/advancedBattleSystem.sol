694a7fa03480425a76e71cfaecebaab2c53c8ad0
mollybeach <mollyjbeach@gmail.com>
2021-09-21 19:36:20 -0700

    loom4  complete changeDna changeName use onlyOwner modifier remove the require statements as they become redundant DRY principles

--- loomFour/advancedBattleSystem.sol
+++ loomFour/advancedBattleSystem.sol
@@ -299,5 +299,82 @@ contract KittyInterface {
     uint256 nextActionAt,
 
 
+Chapter 6: Refactoring Common Logic
+Whoever calls our attack function — we want to make sure the user actually owns the zombie they're attacking with. It would be a security concern if you could attack with someone else's zombie!
 
+Can you think of how we would add a check to see if the person calling this function is the owner of the _zombieId they're passing in?
+
+Give it some thought, and see if you can come up with the answer on your own.
+
+Take a moment... Refer to some of our previous lessons' code for ideas...
+
+Answer below, don't continue until you've given it some thought.
+
+The answer
+We've done this check multiple times now in previous lessons. In changeName(), changeDna(), and feedAndMultiply(), we used the following check:
+
+require(msg.sender == zombieToOwner[_zombieId]);
+This is the same logic we'll need for our attack function. Since we're using the same logic multiple times, let's move this into its own modifier to clean up our code and avoid repeating ourselves.
+
+Put it to the test
+We're back to zombiefeeding.sol, since this is the first place we used that logic. Let's refactor it into its own modifier.
+
+Create a modifier called ownerOf. It will take 1 argument, _zombieId (a uint).
+
+The body should require that msg.sender is equal to zombieToOwner[_zombieId], then continue with the function. You can refer to zombiehelper.sol if you don't remember the syntax for a modifier.
+
+Change the function definition of feedAndMultiply such that it uses the modifier ownerOf.
+
+Now that we're using a modifier, you can remove the line require(msg.sender == zombieToOwner[_zombieId]);
+
+zombiefeeding.sol
+zombieattack.sol
+zombiehelper.sol
+zombiefactory.sol
+ownable.sol
+41424344454647484950515253545540
+  // 2. Add modifier to function definition:
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId) {
+    // 3. Remove this line
+    //require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    require(_isReady(myZombie));
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+      newDna = newDna - newDna % 100 + 99;
+
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiefactory.sol";
+
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+
+    _zombie.readyTime = uint32(now + cooldownTime);
+  }
+
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+      return (_zombie.readyTime <= now);
+  }
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId) {
+    Zombie storage myZombie = zombies[_zombieId];
+    require(_isReady(myZombie));
+
+Zombie Battle System
+
+ Zombie Battle
+
+ Chapter 7: More Refactoring
+We have a couple more places in zombiehelper.sol where we need to implement our new modifier ownerOf.
+
+Put it to the test
+Update changeName() to use ownerOf
+
+Update changeDna() to use ownerOf
 */
--- loomFour/zombiehelper.sol
+++ loomFour/zombiehelper.sol
@@ -31,13 +31,13 @@ contract ZombieHelper is ZombieFeeding {
     zombies[_zombieId].level++;
     }
     // Start here
-    function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
-        require(msg.sender == zombieToOwner[_zombieId]);
+    function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) ownerOf(_zombieId) {
+        //require(msg.sender == zombieToOwner[_zombieId]);
         zombies[_zombieId].name = _newName;
 
     }
-    function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
-        require( msg.sender == zombieToOwner[_zombieId]);
+    function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) ownerOf(_zombieId){
+        //require( msg.sender == zombieToOwner[_zombieId]);
         zombies[_zombieId].dna = _newDna;
     }
     // Create your function here
