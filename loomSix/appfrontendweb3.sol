1b29cc1b1144a52077a15d6d974e92ab10959b8c
mollybeach <mollyjbeach@gmail.com>
2021-09-24 09:21:18 -0700

    loom6 #8 calling payable function sending wei after level up

--- loomSix/appfrontendweb3.sol
+++ loomSix/appfrontendweb3.sol
@@ -253,5 +253,66 @@ erc721.sol
   </head>
   <body>
 
+Chapter 8: Calling Payable Functions
+The logic for attack, changeName, and changeDna will be extremely similar, so they're trivial to implement and we won't spend time coding them in this lesson.
+
+In fact, there's already a lot of repetitive logic in each of these function calls, so it would probably make sense to refactor and put the common code in its own function. (And use a templating system for the txStatus messages — already we're seeing how much cleaner things would be with a framework like Vue.js!)
+
+Let's look at another type of function that requires special treatment in Web3.js — payable functions.
+
+Level Up!
+Recall in ZombieHelper, we added a payable function where the user can level up:
+
+function levelUp(uint _zombieId) external payable {
+  require(msg.value == levelUpFee);
+  zombies[_zombieId].level++;
+}
+The way to send Ether along with a function is simple, with one caveat: we need to specify how much to send in wei, not Ether.
+
+What's a Wei?
+A wei is the smallest sub-unit of Ether — there are 10^18 wei in one ether.
+
+That's a lot of zeroes to count — but luckily Web3.js has a conversion utility that does this for us.
+
+// This will convert 1 ETH to Wei
+web3js.utils.toWei("1");
+In our DApp, we set levelUpFee = 0.001 ether, so when we call our levelUp function, we can make the user send 0.001 Ether along with it using the following code:
+
+cryptoZombies.methods.levelUp(zombieId)
+.send({ from: userAccount, value: web3js.utils.toWei("0.001", "ether") })
+Put it to the Test
+Let's add a levelUp function below feedOnKitty. The code will be very similar to feedOnKitty, but:
+
+The function will take 1 parameter, zombieId
+
+Pre-transaction, it should display the txStatus text "Leveling up your zombie..."
+
+When it calls levelUp on the contract, it should send "0.001" ETH converted toWei, as in the example above
+
+Upon success it should display the text "Power overwhelming! Zombie successfully leveled up"
+
+We don't need to redraw the UI by querying our smart contract with getZombiesByOwner — because in this case we know the only thing that's changed is the one zombie's level.
+
+index.html
+zombieownership.sol
+zombieattack.sol
+zombiehelper.sol
+zombiefeeding.sol
+zombiefactory.sol
+ownable.sol
+safemath.sol
+erc721.sol
+12345678910111213141516171819202122
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>CryptoZombies front-end</title>
+    <script language="javascript" type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script language="javascript" type="text/javascript" src="web3.min.js"></script>
+    <script language="javascript" type="text/javascript" src="cryptozombies_abi.js"></script>
+  </head>
+  <body>
+
 
 */
